9a4125bdfb0381050457b3adc6001d6e9961cb4f
Diego Guemes <diegogp1988@gmail.com>
2013-08-27 14:20:59 +0200

    Remove duplicate test code

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -3,15 +3,17 @@ require './fizz_buzz'
 
 describe 'FizzBuzz' do
 
+  before :each do
+    @fizz_buzz =FizzBuzz.new
+  end
+
   it 'says Fizz when number is divisible by 3' do
-    fizz_buzz = FizzBuzz.new
-    fizz_buzz.play(3).should eq('Fizz')
-    fizz_buzz.play(6).should eq('Fizz')
+    @fizz_buzz.play(3).should eq('Fizz')
+    @fizz_buzz.play(6).should eq('Fizz')
   end
 
   it 'says Buzz when number is divisible by 5' do
-    fizz_buzz = FizzBuzz.new
-    fizz_buzz.play(5).should eq('Buzz')
-    fizz_buzz.play(10).should eq('Buzz')
+    @fizz_buzz.play(5).should eq('Buzz')
+    @fizz_buzz.play(10).should eq('Buzz')
   end
 end
