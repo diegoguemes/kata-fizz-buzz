9f29868b663f4ccb81b30f37e587ca7c76237ee5
Diego Guemes <diegogp1988@gmail.com>
2013-08-27 14:12:06 +0200

    Say buzz test failing

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -8,4 +8,10 @@ describe 'FizzBuzz' do
     fizz_buzz.play(3).should eq('Fizz')
     fizz_buzz.play(6).should eq('Fizz')
   end
+
+  it 'says Buzz when number is divisible by 5' do
+    fizz_buzz = FizzBuzz.new
+    fizz_buzz.play(5).should eq('Buzz')
+    fizz_buzz.play(10).should eq('Buzz')
+  end
 end
