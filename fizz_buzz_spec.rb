fbdafdf6b487d5e433948eab6d3c4c32dcb95c94
Diego Guemes <diegogp1988@gmail.com>
2013-08-27 14:48:13 +0200

    Test refactoring

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -7,13 +7,15 @@ describe 'FizzBuzz' do
     @fizz_buzz =FizzBuzz.new
   end
 
-  it 'says Fizz when number is divisible by 3' do
-    @fizz_buzz.play(3).should eq('Fizz')
-    @fizz_buzz.play(6).should eq('Fizz')
-  end
+  describe 'says Fizz when number' do
+    it 'is divisible by 3' do
+      @fizz_buzz.play(3).should eq('Fizz')
+      @fizz_buzz.play(6).should eq('Fizz')
+    end
 
-  it 'says Fizz when number contains a 3' do
-    @fizz_buzz.play(13).should eq('Fizz')
+    it 'contains a 3' do
+      @fizz_buzz.play(13).should eq('Fizz')
+    end
   end
 
   it 'says Buzz when number is divisible by 5' do
@@ -21,11 +23,11 @@ describe 'FizzBuzz' do
     @fizz_buzz.play(10).should eq('Buzz')
   end
 
-  it 'appends words when number is divisible by different numbers' do
+  it 'appends words when number satisfies different rules' do
     @fizz_buzz.play(15).should eq('FizzBuzz')
   end
 
-  it 'says the number when number is not divisible by 3 or 5' do
+  it 'says the number when number does not satisfy any rule' do
     @fizz_buzz.play(11).should eq('11')
   end
 end
